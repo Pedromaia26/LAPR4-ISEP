37be8617ffe75aaff27958d1e5728051115c05a4
1201276_TiagoMarques <1201276@isep.ipp.pt>
2022-05-25 01:31:42 +0100

    #21 Configuring AgvDigitalTwin Sever to later answer the request of the AGV Manager

---
+++ Projeto Java/base.daemon.agvDigitalTwin/src/main/java/daemon/agvdigitaltwin/AGVDigitalTwinDaemon.java
@@ -0,0 +1,45 @@
+package daemon.agvdigitaltwin;
+
+import com.fasterxml.jackson.core.Base64Variant;
+import daemon.agvdigitaltwin.presentation.AgvDigitalTwinTcpServer;
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.usermanagement.domain.BasePasswordPolicy;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.infrastructure.authz.domain.model.PlainTextEncoder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class AGVDigitalTwinDaemon {
+
+    // TODO read port number from property file
+    private static final int BOOKING_PORT = 8897;
+    private static final Logger LOGGER = LogManager.getLogger(AGVDigitalTwinDaemon.class);
+
+    /**
+     * Avoid instantiation of this class.
+     */
+    private AGVDigitalTwinDaemon() {
+    }
+
+    public static void main(final String[] args) throws UnknownHostException {
+
+
+        LOGGER.info("Configuring the daemon");
+        System.out.println(InetAddress.getLocalHost().getHostAddress());
+        AuthzRegistry.configure(PersistenceContext.repositories().users(),
+                new BasePasswordPolicy(),
+                new PlainTextEncoder());
+
+        LOGGER.info("Starting the server socket");
+        final var server = new AgvDigitalTwinTcpServer();
+        server.start(BOOKING_PORT, true);
+
+        LOGGER.info("Exiting the daemon");
+        System.exit(0);
+    }
+}
