ea2fdcbb908cbd13d29f19536904bda001f9886f
pedrompf <1201384@isep.ipp.pt>
2022-05-28 23:34:19 +0100

    #23 Adding dashboard files to run in a browser

---
+++ Projeto Java/base.core/src/main/java/eapli/base/dashboardmanagement/www/rcomp-ajax.js
@@ -0,0 +1,38 @@
+function loadWarehouse() {
+	var request = new XMLHttpRequest();
+    var vWarehouse=document.getElementById("warehouse");
+
+    request.onload = function() {
+        vWarehouse.innerHTML = this.responseText;
+        vWarehouse.style.color="black";
+
+        setTimeout(loadWarehouse, 15000);
+    };
+
+    request.ontimeout = function() {
+        vWarehouse.innerHTML = "Server timeout, still trying ...";
+        vWarehouse.style.color="red";
+        setTimeout(loadWarehouse, 15000);
+    };
+
+    request.onerror = function() {
+        vWarehouse.innerHTML = "No server reply, still trying ...";
+        vWarehouse.style.color="red";
+        setTimeout(loadWarehouse, 15000);
+    };
+
+    request.open("GET", "/warehouse", true);
+    request.timeout = 15000;
+    request.send();
+}
+
+function voteFor(option) {
+	var request = new XMLHttpRequest();
+  	request.open("PUT", "/votes/" + option , true);
+  	request.send();
+    var vWarehouse=document.getElementById("warehouse");
+    vBoard.innerHTML = vBoard.innerHTML + "<p>Casting your vote ... Please wait.";
+
+}
+	
+
