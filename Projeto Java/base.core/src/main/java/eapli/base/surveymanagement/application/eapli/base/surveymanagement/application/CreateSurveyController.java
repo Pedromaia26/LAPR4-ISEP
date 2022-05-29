62b10a9a62326889ef6a74132ba7d434b731448e
1201276_TiagoMarques <1201276@isep.ipp.pt>
2022-05-29 19:50:43 +0100

    #19 Handling ANTLR error while parsing the survey

--- Projeto Java/base.core/src/main/java/eapli/base/surveymanagement/application/eapli/base/surveymanagement/application/CreateSurveyController.java
+++ Projeto Java/base.core/src/main/java/eapli/base/surveymanagement/application/eapli/base/surveymanagement/application/CreateSurveyController.java
@@ -3,6 +3,8 @@ package eapli.base.surveymanagement.application.eapli.base.surveymanagement.appl
 import java.io.*;
 
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.tree.*;
 import eapli.base.surveymanagement.domain.Survey;
 import eapli.base.usermanagement.domain.BaseRoles;
@@ -12,9 +14,10 @@ import eapli.framework.infrastructure.authz.application.AuthzRegistry;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.BitSet;
 import java.util.List;
 
-public class CreateSurveyController {
+public class CreateSurveyController implements ANTLRErrorListener{
     private final AuthorizationService authz = AuthzRegistry.authorizationService();
 
     public Survey createSurvey(final String file) throws IOException {
@@ -24,9 +27,31 @@ public class CreateSurveyController {
         SurveyLexer lexer = new SurveyLexer(new ANTLRInputStream(fis));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         SurveyParser parser = new SurveyParser(tokens);
+        ANTLRErrorListener errorListener = new CreateSurveyController();
+        parser.addErrorListener(errorListener);
         ParseTree tree = parser.prog(); // parse
         SurvVisitor surv = new SurvVisitor();
 
         return surv.visit(tree);
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object o, int i, int i1, String s, RecognitionException e) {
+        System.out.println("Syntax Error");
+    }
+
+    @Override
+    public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, boolean b, BitSet bitSet, ATNConfigSet atnConfigSet) {
+        System.out.println("Report ambiguity");
+    }
+
+    @Override
+    public void reportAttemptingFullContext(Parser parser, DFA dfa, int i, int i1, BitSet bitSet, ATNConfigSet atnConfigSet) {
+        System.out.println("Report attempting full context");
+    }
+
+    @Override
+    public void reportContextSensitivity(Parser parser, DFA dfa, int i, int i1, int i2, ATNConfigSet atnConfigSet) {
+        System.out.println("Report context sensitivity");
     }
 }
