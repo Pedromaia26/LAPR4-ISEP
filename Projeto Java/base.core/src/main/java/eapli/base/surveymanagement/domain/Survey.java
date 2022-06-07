9e7885dc873626384684f36cf41e4a47746237b2
Pedro_Maia <1201381@isep.ipp.pt>
2022-06-07 04:56:56 +0100

    #29 Updating survey class to permit the client answer

--- Projeto Java/base.core/src/main/java/eapli/base/surveymanagement/domain/Survey.java
+++ Projeto Java/base.core/src/main/java/eapli/base/surveymanagement/domain/Survey.java
@@ -1,20 +1,65 @@
 package eapli.base.surveymanagement.domain;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import eapli.base.clientusermanagement.domain.ClientUser;
+import eapli.base.surveymanagement.domain.dto.SurveyDTO;
+import eapli.framework.domain.model.AggregateRoot;
+import eapli.framework.domain.model.DomainEntities;
 
-public class Survey {
+import javax.persistence.*;
+import java.util.*;
 
+@Entity
+public class Survey implements AggregateRoot<SurveyIdentifier> {
+
+    @EmbeddedId
     private SurveyIdentifier id;
 
+    @Embedded
     private Title title;
 
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "message",
+                    column = @Column(name = "WelcomeMessage")),
+    })
     private Message welcomeMessage;
 
-    private Set<Section> sections = new HashSet<>();
+    @ElementCollection
+    private List<SurveySection> sections = new ArrayList<>();
 
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "message",
+                    column = @Column(name = "FinalMessage")),
+    })
     private Message finalMessage;
+
+    @ElementCollection
+    private List<ClientUserSurvey> clientUsersToAnswer = new ArrayList<>();
+
+    @ElementCollection
+    private List<ClientUserSurvey> clientUsersAnswered = new ArrayList<>();
+
+    @ManyToOne
+    private Context context;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "age",
+                    column = @Column(name = "MinimumAge")),
+    })
+    private Age minAge;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "age",
+                    column = @Column(name = "MaximumAge")),
+    })
+    private Age maxAge;
+
+    public Message getWelcomeMessage() {
+        return welcomeMessage;
+    }
 
     public Survey(SurveyIdentifier identifier, Title title, Message welcomeMessage, Message finalMessage){
 
@@ -71,12 +116,28 @@ public class Survey {
         this.finalMessage = finalMessage;
     }
 
-    public boolean addSection(final Section section) {
-        return sections.add(section);
+    public boolean addSection(final SurvSection section) {
+        return sections.add(new SurveySection(section));
     }
 
-    public Set<Section> questions() {
-        return Collections.unmodifiableSet(sections);
+    public List<SurveySection> sections() {
+        return sections;
+    }
+
+    public boolean addClientUserToAnswer(final ClientUser clientUser) {
+        return clientUsersToAnswer.add(new ClientUserSurvey(clientUser));
+    }
+
+    public List<ClientUserSurvey> clientUsersToAnswer() {
+        return clientUsersToAnswer;
+    }
+
+    public boolean addClientUserAnswered(final ClientUser clientUser) {
+        return clientUsersAnswered.add(new ClientUserSurvey(clientUser));
+    }
+
+    public List<ClientUserSurvey> clientUsersAnswered() {
+        return clientUsersAnswered;
     }
 
     @Override
@@ -84,8 +145,53 @@ public class Survey {
         return "--- Survey ---" +
                 "\nId: " + id +
                 "\nTitle: " + title +
+                "\nMinAge: " + minAge +
+                "\nMaxAge: " + maxAge +
                 "\nWelcome Message: " + welcomeMessage + "\n" +
                 sections +
                 "\nFinal Message: " + finalMessage + "\n";
+    }
+
+    @Override
+    public boolean sameAs(Object other) {
+        return DomainEntities.areEqual(this, other);
+    }
+
+    @Override
+    public SurveyIdentifier identity() {
+        return id;
+    }
+
+    public Context context() {
+        return context;
+    }
+
+    public void modifyContext(Context context) {
+        this.context = context;
+    }
+
+    public Age minAge() {
+        return minAge;
+    }
+
+    public void modifyMinAge(Age minAge) {
+        this.minAge = minAge;
+    }
+
+    public Age maxAge() {
+        return maxAge;
+    }
+
+    public void modifyMaxAge(Age maxAge) {
+        this.maxAge = maxAge;
+    }
+
+    public SurveyDTO toDTO(){
+        if (welcomeMessage == null){
+            return new SurveyDTO(id.toString(), title.toString(), finalMessage.toString(), minAge.age(), maxAge.age());
+        }
+        else{
+            return new SurveyDTO(id.toString(), title.toString(), welcomeMessage.toString(), finalMessage.toString(), minAge.age(), maxAge.age());
+        }
     }
 }
