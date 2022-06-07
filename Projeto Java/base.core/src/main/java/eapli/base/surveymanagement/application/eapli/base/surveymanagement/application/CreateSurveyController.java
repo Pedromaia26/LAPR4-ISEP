a5ee8fcf1ff6ab379431eeb7b06a4bc7fbeb45a2
Pedro_Maia <1201381@isep.ipp.pt>
2022-06-07 04:49:09 +0100

    #29 Controller that permits to create a survey with a context

--- Projeto Java/base.core/src/main/java/eapli/base/surveymanagement/application/eapli/base/surveymanagement/application/CreateSurveyController.java
+++ Projeto Java/base.core/src/main/java/eapli/base/surveymanagement/application/eapli/base/surveymanagement/application/CreateSurveyController.java
@@ -2,11 +2,21 @@ package eapli.base.surveymanagement.application.eapli.base.surveymanagement.appl
 
 import java.io.*;
 
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.ordermanagement.domain.Quantity;
+import eapli.base.productmanagement.domain.Product;
+import eapli.base.shoppingcartmanagement.domain.ShoppingCart;
+import eapli.base.shoppingcartmanagement.domain.ShoppingCartLine;
+import eapli.base.surveymanagement.domain.*;
+import eapli.base.surveymanagement.repositories.ContextRepository;
+import eapli.base.surveymanagement.repositories.QuestionRepository;
+import eapli.base.surveymanagement.repositories.SectionRepository;
+import eapli.base.surveymanagement.repositories.SurveyRepository;
+import eapli.framework.domain.repositories.TransactionalContext;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.atn.ATNConfigSet;
 import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.tree.*;
-import eapli.base.surveymanagement.domain.Survey;
 import eapli.base.usermanagement.domain.BaseRoles;
 import eapli.framework.infrastructure.authz.application.AuthorizationService;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
@@ -19,10 +29,64 @@ import java.util.List;
 
 public class CreateSurveyController implements ANTLRErrorListener{
     private final AuthorizationService authz = AuthzRegistry.authorizationService();
+    private final TransactionalContext txCtx = PersistenceContext.repositories().newTransactionalContext();
+    private final SurveyRepository surveyRepository = PersistenceContext.repositories().surveys(txCtx);
+    private final SectionRepository sectionRepository = PersistenceContext.repositories().sections(txCtx);
+    private final QuestionRepository questionRepository = PersistenceContext.repositories().questions(txCtx);
+    private final ContextRepository contextRepository = PersistenceContext.repositories().contexts();
 
-    public Survey createSurvey(final String file) throws IOException {
+    public Survey createSurvey(final String file, final Long context) throws IOException {
         authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.SALES_MANAGER, BaseRoles.POWER_USER, BaseRoles.ADMIN);
 
+        Survey survey = parseSurvey(file);
+        survey.modifyContext(contextRepository.findContextById(context));
+        try{
+            txCtx.beginTransaction();
+            for (SurveySection s : survey.sections()){
+                for (SectionQuestion q : s.section().questions()){
+                    questionRepository.save(q.question());
+                }
+                sectionRepository.save(s.section());
+            }
+            surveyRepository.save(survey);
+            txCtx.commit();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            txCtx.rollback();
+            throw new IllegalArgumentException();
+        }
+
+        return survey;
+    }
+
+    public Survey createSurvey(final String file, final Long context, int minAge, int maxAge) throws IOException {
+        authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.SALES_MANAGER, BaseRoles.POWER_USER, BaseRoles.ADMIN);
+
+
+        Survey survey = parseSurvey(file);
+        survey.modifyMinAge(new Age(minAge));
+        survey.modifyMaxAge(new Age(maxAge));
+        survey.modifyContext(contextRepository.findContextById(context));
+        try{
+            txCtx.beginTransaction();
+            for (SurveySection s : survey.sections()){
+                for (SectionQuestion q : s.section().questions()){
+                    questionRepository.save(q.question());
+                }
+                sectionRepository.save(s.section());
+            }
+            surveyRepository.save(survey);
+            txCtx.commit();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            txCtx.rollback();
+            throw new IllegalArgumentException();
+        }
+
+        return survey;
+    }
+
+    public Survey parseSurvey(final String file) throws IOException {
         FileInputStream fis = new FileInputStream(file);
         SurveyLexer lexer = new SurveyLexer(new ANTLRInputStream(fis));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -32,7 +96,7 @@ public class CreateSurveyController implements ANTLRErrorListener{
         ParseTree tree = parser.prog(); // parse
         SurvVisitor surv = new SurvVisitor();
 
-        return surv.visit(tree);
+       return surv.visit(tree);
     }
 
     @Override
