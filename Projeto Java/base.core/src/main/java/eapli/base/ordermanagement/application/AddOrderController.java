60062745aa2118b6d3bb9e9faa67f31845c1ee8e
Pedro_Maia <1201381@isep.ipp.pt>
2022-06-09 00:13:50 +0100

    #40 Adding controller method that is called when a user makes an order

--- Projeto Java/base.core/src/main/java/eapli/base/ordermanagement/application/AddOrderController.java
+++ Projeto Java/base.core/src/main/java/eapli/base/ordermanagement/application/AddOrderController.java
@@ -9,6 +9,7 @@ import eapli.base.agvmanagement.repositories.AGVRepository;
 import eapli.base.clientusermanagement.domain.BillingPostalAddresses;
 import eapli.base.clientusermanagement.domain.ClientUser;
 import eapli.base.clientusermanagement.domain.DeliveringPostalAddresses;
+import eapli.base.clientusermanagement.domain.VAT;
 import eapli.base.clientusermanagement.repositories.ClientUserRepository;
 import eapli.base.infrastructure.persistence.PersistenceContext;
 import eapli.base.ordermanagement.domain.*;
@@ -16,14 +17,22 @@ import eapli.base.ordermanagement.repositories.OrderLineRepository;
 import eapli.base.ordermanagement.repositories.OrderRepository;
 import eapli.base.orderstatusmanagement.domain.Status;
 import eapli.base.orderstatusmanagement.repositories.StatusRepository;
+import eapli.base.productmanagement.domain.Product;
+import eapli.base.shoppingcartmanagement.domain.Line;
+import eapli.base.shoppingcartmanagement.domain.ShoppingCart;
+import eapli.base.shoppingcartmanagement.domain.ShoppingCartLine;
+import eapli.base.shoppingcartmanagement.repositories.ShoppingCartRepository;
 import eapli.base.taskmanagement.application.TasksListService;
 import eapli.base.taskmanagement.domain.Task;
 import eapli.base.usermanagement.domain.BaseRoles;
 import eapli.framework.application.UseCaseController;
+import eapli.framework.domain.repositories.TransactionalContext;
 import eapli.framework.infrastructure.authz.application.AuthorizationService;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.infrastructure.authz.domain.model.Username;
 import eapli.framework.time.util.Calendars;
 
+import java.util.Optional;
 import java.util.Set;
 
 @UseCaseController
@@ -33,13 +42,14 @@ public class AddOrderController {
     private final ClientUserRepository userRepository = PersistenceContext.repositories().clientUsers();
     private final StatusRepository statusRepository = PersistenceContext.repositories().status();
     private final OrderLineRepository orderLineRepository = PersistenceContext.repositories().orderlines();
+    private final TransactionalContext txCtx = PersistenceContext.repositories().newTransactionalContext();
+    private final ShoppingCartRepository shoppingCartRepository = PersistenceContext.repositories().shoppingCarts(txCtx);
     private TasksListService svcTask = new TasksListService();
     private AGVListService svcAGV = new AGVListService();
     private final AssignAGVService assignAGVService = new AssignAGVService();
     private AGV agv;
 
     public ProductOrder addOrder(final String clientvat) {
-        authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.SALES_CLERK, BaseRoles.POWER_USER, BaseRoles.ADMIN);
         ClientUser clientUser = userRepository.findByVAT(clientvat);
         long statusid = 1;
         Status status = statusRepository.findByStatusId(statusid);
@@ -49,7 +59,6 @@ public class AddOrderController {
 
     public boolean addOrder(final String clientvat, final ProductOrder order, final Set<String[]> deliveringPostalAddress, final Set<String[]> billingPostalAddress,
                                  final String shipmentMethod, final double shipmentCost, final String paymentMethod) {
-        authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.SALES_CLERK);
 
         ClientUser clientUser = userRepository.findByVAT(clientvat);
         long statusid = 1;
@@ -72,7 +81,33 @@ public class AddOrderController {
         return true;
     }
 
+    public boolean addOrder(final Set<String[]> deliveringPostalAddress, final Set<String[]> billingPostalAddress,
+                            final String shipmentMethod, final double shipmentCost, final String paymentMethod) {
 
+        ShoppingCart shoppingCart = shoppingCartRepository.findByVat(getUserSessionVat().vat());
+        ProductOrder order = addOrder(getUserSessionVat().vat());
+        for (Line shoppingCartLine : shoppingCart.shoppingCartLines()){
+            orderLineRepository.save(new OrderLine(shoppingCartLine.shoppingCartLine().Product(), shoppingCartLine.shoppingCartLine().Quantity(), order, new Cost(shoppingCartLine.shoppingCartLine().UnitPrice().getPrice())));
+        }
 
+        addOrder(getUserSessionVat().vat(), order, deliveringPostalAddress, billingPostalAddress,
+                shipmentMethod, shipmentCost, paymentMethod);
 
+        return true;
+    }
+
+    private VAT getUserSessionVat(){
+        return getUser().identity();
+    }
+
+    private ClientUser getUser(){
+        Username username = authz.session().get().authenticatedUser().username();
+        Optional<ClientUser> client = userRepository.findByUsername(username);
+        return client.get();
+    }
+
+    public boolean verifyOrder(){
+        ShoppingCart shoppingCart = shoppingCartRepository.findByVat(getUserSessionVat().vat());
+        return shoppingCart.shoppingCartLines().size() > 0;
+    }
 }
