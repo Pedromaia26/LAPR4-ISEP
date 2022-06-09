1dde6986c20b2fdfc2b9e8e29930807583f742a7
1201276_TiagoMarques <1201276@isep.ipp.pt>
2022-06-09 14:44:32 +0100

    #38 updating client and server certificates

--- Projeto Java/base.core/src/main/java/eapli/base/ordermanagement/application/ViewClientOrdersService.java
+++ Projeto Java/base.core/src/main/java/eapli/base/ordermanagement/application/ViewClientOrdersService.java
@@ -13,17 +13,18 @@ import java.util.Arrays;
 public class ViewClientOrdersService {
     private InetAddress serverIP;
     private SSLSocket sock;
-    private static final String TRUSTED_STORE = "certificates/server.jks";
+    private static final String TRUSTED_STORE_SERVER = "certificates/server.jks";
+    private static final String TRUSTED_STORE_CLIENT = "certificates/client.jks";
     private static final String KEYSTORE_PASS = "Password1";
 
     public String viewClientOrdersService(String clientVat) {
 
         //Trust this cert provided by server
-        System.setProperty("javax.net.ssl.trustStore", TRUSTED_STORE);
+        System.setProperty("javax.net.ssl.trustStore", TRUSTED_STORE_SERVER);
         System.setProperty("javax.net.ssl.trustStorePassword", KEYSTORE_PASS);
 
         // Use this certificate and private key for client certificate when requested by the server
-        System.setProperty("javax.net.ssl.keyStore", TRUSTED_STORE);
+        System.setProperty("javax.net.ssl.keyStore", TRUSTED_STORE_CLIENT);
         System.setProperty("javax.net.ssl.keyStorePassword",KEYSTORE_PASS);
 
         SSLSocketFactory sf = (SSLSocketFactory) SSLSocketFactory.getDefault();
--- Projeto Java/base.daemon.orderServer/src/main/java/daemon/ordermanager/presentation/OrderManagerTcpServer.java
+++ Projeto Java/base.daemon.orderServer/src/main/java/daemon/ordermanager/presentation/OrderManagerTcpServer.java
@@ -17,7 +17,8 @@ public class OrderManagerTcpServer {
 
 
     private static final Logger LOGGER = LogManager.getLogger(OrderManagerTcpServer.class);
-    private static final String TRUSTED_STORE = "certificates/server.jks";
+    private static final String TRUSTED_STORE_SERVER = "certificates/server.jks";
+    private static final String TRUSTED_STORE_CLIENT = "certificates/client.jks";
     private static final String KEYSTORE_PASS = "Password1";
 
     private static class OrderManagerHandler extends Thread {
@@ -100,12 +101,12 @@ public class OrderManagerTcpServer {
 
         //Trust the cert provided by authorized clients
 
-        System.setProperty("javax.net.ssl.trustStore", TRUSTED_STORE);
+        System.setProperty("javax.net.ssl.trustStore", TRUSTED_STORE_CLIENT);
         System.setProperty("javax.net.ssl.trustStorePassword",KEYSTORE_PASS);
 
 
         //Use this certificate and private key as Server certificate
-        System.setProperty("javax.net.ssl.keyStore",TRUSTED_STORE);
+        System.setProperty("javax.net.ssl.keyStore",TRUSTED_STORE_SERVER);
         System.setProperty("javax.net.ssl.keyStorePassword",KEYSTORE_PASS);
 
         SSLServerSocketFactory sslF = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
