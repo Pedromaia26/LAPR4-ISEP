26f72048873fc2907df0f57cc2f828dec6158e8f
1201371_EduardoSilva <1201371@isep.ipp.pt>
2022-06-16 17:48:42 +0100

    #33 creating a request for the agvs in maintenance

---
+++ Projeto Java/base.daemon.agvManager/src/main/java/agvmanager/tcpprotocol/server/MaintenanceAGVRequest.java
@@ -0,0 +1,48 @@
+package agvmanager.tcpprotocol.server;
+
+import eapli.base.agvmanagement.application.AGVManagerController;
+import eapli.base.agvmanagement.application.AGVManagerControllerImpl;
+import eapli.base.communicationprotocol.CommunicationProtocol;
+
+public class MaintenanceAGVRequest extends AgvManagerProtocolRequest{
+
+    private AGVManagerController agvManagerController = new AGVManagerControllerImpl();
+    private String agvID;
+
+    protected MaintenanceAGVRequest(AGVManagerController controller, String inputData) {
+        super(controller, inputData);
+        agvID=inputData;
+    }
+
+
+    @Override
+    public String execute() {
+
+        String response;
+
+        // execution
+        try{
+            agvManagerController.AGVinMaintenance(agvID);
+            response = buildResponse();
+        }catch (Exception e){
+            response = e.getMessage();
+
+        }
+        // response
+        return response;
+
+    }
+
+    @Override
+    public byte[] outputProtocol() {
+
+        byte[] dataLength = CommunicationProtocol.dataLengthCalculator(buildResponse());
+        byte[] array = new byte[]{CommunicationProtocol.PROTOCOL_V1,
+                CommunicationProtocol.UPDATE_AGV_STATUS_MAINTENANCE_RESPONSE_CODE, dataLength[0], dataLength[1]};
+        return array;
+    }
+
+    private String buildResponse() {
+        return String.format("AGV %s is in maintenance.", agvID);
+    }
+}
